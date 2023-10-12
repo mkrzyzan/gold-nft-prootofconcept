2c43e3dbc9e0527de5e193ef8676d0cc816bd2ab
Marcin Krzyzanowski <marcin.adam.k@gmail.com>
2023-10-12 23:39:42 +0800

    batch rpc call for toekns owners

--- server.js
+++ server.js
@@ -23,15 +23,39 @@ app.get('/api', async (req, res) => {
     // Calling the getMintedNfts method
     let mintedNfts = await alchemy.nft.getMintedNfts(address, options);
 
-    // token owned by the burned address by getNftsForOwner
-    let burnedNfts = await alchemy.nft.getNftsForOwner("0x0000000000000000000000000000000000000000", options);
+    // create an array of rpcEthCallRequests
+    let rpcEthCallRequests = mintedNfts.nfts.map(nft => ({
+        jsonrpc: "2.0",
+        method: "eth_call",
+        params: [{
+            to: "0x2C6B7EEc10c794cCBE7234Aa33aD841BD7694BDa",
+            data: "0x6352211e" + parseInt(nft.tokenId).toString(16).padStart(64, '0')
+        }, "latest"],
+        id: nft.tokenId
+    }));
 
-    // iterate through burnedNfts and add tokenId to Set
-    let burnedTokenIds = new Set();
-    burnedNfts.ownedNfts.forEach(nft => burnedTokenIds.add(nft.tokenId));
+    // fetch POST request
+    let rpcEthCallResponses = await fetch("https://eth-sepolia.g.alchemy.com/v2/FMzA-jPzQaGyIwIAcJW8WIHlJvIOTAv9", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(rpcEthCallRequests)
+    });
 
-    // filter out all mintedNfts that are not owned by the burned address
-    mintedNfts.nfts = mintedNfts.nfts.filter(nft => !burnedTokenIds.has(nft.tokenId));
+    // convert the response to JSON
+    repJson = await rpcEthCallResponses.json();
+
+    console.log(JSON.stringify(repJson, null, 2));
+
+    // group data in repJson by "id" field
+    let repJsonById = {};
+    repJson.forEach(rep => repJsonById[rep.id] = rep.result);
+
+    console.log(JSON.stringify(repJsonById, null, 2));
+
+    // filter out all mintedNfts which tokenId don't exists in repJsonById
+    mintedNfts.nfts = mintedNfts.nfts.filter(nft => repJsonById[nft.tokenId]);
 
     // send back the response to the client with the mintedNfts
     res.send(mintedNfts);
